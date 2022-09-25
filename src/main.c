b1b159fec86cbb55e716f9626cf19434a45cec2a
Michaël Lammerts van Bueren <69117774+Michlvb@users.noreply.github.com>
2022-09-25 13:33:44 +0200

    Polished ctrl d option a bit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dtran <dtran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/23 13:30:43 by dtran         #+#    #+#                 */
-/*   Updated: 2022/09/24 16:39:32 by mlvb          ########   odam.nl         */
+/*   Updated: 2022/09/25 13:33:05 by mlvb          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	*input;
+	extern char* rl_line_buffer;
 	
 	init_signals();
 	(void)argc;
@@ -24,10 +25,14 @@ int	main(int argc, char *argv[], char *envp[])
 	{
 		input = readline("Dit is echt leuk: ");
 		if (!input)
-			exit(EXIT_FAILURE);
+		{
+			// When ctrl + D is pushed, this will put out the message that it has stopped.
+			ft_putendl_fd("exit", 1);
+			exit(0);
+		}
 		if (input && *input)
 			add_history(input);
-		// ft_snorlexer(input);
+		ft_snorlexer(input);
 		free(input);
 		input = NULL;
 	}
